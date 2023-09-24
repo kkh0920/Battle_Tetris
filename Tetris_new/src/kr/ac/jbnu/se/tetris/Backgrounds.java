fcdbd8e497c3d09b8b4c0078ef2d5dc10c5a1a35
notyourocean <104780993+notyourocean@users.noreply.github.com>
2023-09-25 04:21:58 +0900

    Setting 화면 생성 + 설명 추가, 배경화면 클래스 분리

---
+++ Tetris_new/src/kr/ac/jbnu/se/tetris/Backgrounds.java
@@ -0,0 +1,35 @@
+package kr.ac.jbnu.se.tetris;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class Backgrounds extends JFrame {
+    int Frame_X = 430, Frame_Y = 336;
+    BufferedImage img = null;
+    BackG background;
+    JLayeredPane frame = new JLayeredPane();
+    Backgrounds() throws IOException {
+        DrawBackGround();
+    }
+
+    public void DrawBackGround() throws IOException { // LayerdPane에 이미지를 덮어씌우는 메소드
+        frame.setSize(Frame_X,Frame_Y);
+        frame.setLayout(null);
+
+        img = ImageIO.read(new File("image\\backg.png"));
+
+        background = new BackG();
+        background.setSize(Frame_X,Frame_Y);
+        frame.add(background);
+    }
+
+    class BackG extends JPanel { // Panel에 이미지를 나타내기 위해서,
+        public void paint(Graphics g){
+            g.drawImage(img, 0,0,null);
+        }
+    }
+}
--- Tetris_new/src/kr/ac/jbnu/se/tetris/Setting.java
+++ Tetris_new/src/kr/ac/jbnu/se/tetris/Setting.java
@@ -1,15 +1,73 @@
 package kr.ac.jbnu.se.tetris;
 
+import javafx.scene.layout.Background;
+import sun.audio.AudioPlayer;
+import sun.audio.AudioStream;
+
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
 
 public class Setting extends JFrame {
-    
-    Setting() {
-        setSize(1000,563);
+
+    Backgrounds backgrounds;
+    JButton musicOn,musicOff,changekey;
+    Button bt;
+
+    //AudioStream BGM; 실질적인 배경음악이 들어가는 객체
+    //AudioPlayer a = AudioPlayer.player; //-> AudioPlayer, 배경음악 재생시 필요한 객체
+
+
+
+
+    Setting() throws IOException {
+        setFrame();
+    }
+
+    public void setFrame() throws IOException {
+        backgrounds = new Backgrounds();
+        setButton();
+        add(musicOn); add(musicOff); add(changekey); add(backgrounds.frame);
+        setSize(430,336);
         setLayout(null);
         setResizable(false);
-        setVisible(true);
         setLocationRelativeTo(null);
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);/*
+        musicOn.addActionListener(new AudioSetting());
+        musicOff.addActionListener(new AudioSetting());*/ // 배경화면 on off를 위한 버튼 액션 추가
+    }
+
+    public void setButton() {
+        musicOn = new JButton(new ImageIcon("image\\musicOn.png"));
+        bt = new Button(musicOn);
+        musicOn.setBounds(50,30,100,100);
+
+        musicOff = new JButton(new ImageIcon("image\\musicOff.png"));
+        bt = new Button(musicOff);
+        musicOff.setBounds(50,180,100,100);
+
+        changekey = new JButton("키 변경");
+        changekey.setBounds(250,80,150,150);
+
+          changekey.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // 새로운 프레임 생성, 입력 box에 값 입력 후, 값이 들어왔을 경우 키 값 변경 (키 값 = 다른 클래스에서 변수)
+                // 완료 button을 누를 경우 변경 frame은 visible false상태로 전환
+            }
+        });
+
+    }
+    class AudioSetting implements ActionListener { // 배경음악을 키고 끌 수 있게 해주는 클래스
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            if(e.getSource() == musicOn) {
+                //a.start(); -> Music Start
+            }
+            else if(e.getSource() == musicOff){
+                // a.stop(); -> Music Stop
+            }
+        }
     }
 }
