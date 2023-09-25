ea888e568fcae34347a5e847be3793a8325c646b
kkh0920 <rudgns1535@gmail.com>
2023-09-25 15:21:22 +0900

    선택화면

--- Tetris_new/src/kr/ac/jbnu/se/tetris/Select.java
+++ Tetris_new/src/kr/ac/jbnu/se/tetris/Select.java
@@ -1,9 +1,12 @@
 package kr.ac.jbnu.se.tetris;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
 
 class Button {
@@ -20,19 +23,29 @@ public class Select extends JFrame {
     JButton ai,versus,setting,tutorial;
     Button bt;
     BufferedImage img = null;
-   // BackG background;
+    BackG background;
     JLayeredPane frame = new JLayeredPane();
-    Backgrounds backgrounds;
 
     Select() throws IOException {
         setFrame();
     }
-    public void setFrame() throws IOException {
-        //drawBackground();
-        backgrounds = new Backgrounds();
 
+    public void drawBackground() throws IOException { // ImageIO.read => 예외처리 -> IOException이 필수
+        frame.setSize(Frame_X,Frame_Y);
+        frame.setLayout(null);
+
+        // img = ImageIO.read(new File("TetrisCode/image/backg.png"));
+        img = ImageIO.read(new File("image\\backg.png"));
+
+        background = new BackG();
+        background.setSize(Frame_X,Frame_Y);
+        frame.add(background);
+    }
+
+    public void setFrame() throws IOException {
+        drawBackground();
         setButton();
-        add(ai); add(versus); add(setting);add(tutorial);add(backgrounds.frame);
+        add(ai); add(versus); add(setting);add(tutorial);add(frame);
         setSize(Frame_X,Frame_Y);
         setLayout(null);
         setResizable(false);
@@ -42,18 +55,23 @@ public class Select extends JFrame {
     }
 
     public void setButton() {
+        
+        // ai = new JButton(new ImageIcon("TetrisCode/image/AImodes.png"));
         ai = new JButton(new ImageIcon("image\\AImodes.png"));
         ai.setBounds(250,-20,Bt_W,Bt_H);
         bt = new Button(ai);
 
-        versus = new JButton(new ImageIcon("Image\\2P_modes.png"));
+        // versus = new JButton(new ImageIcon("TetrisCode/image/2P_modes.png"));
+        versus = new JButton(new ImageIcon("image\\2P_modes.png"));
         versus.setBounds(250,80,Bt_W,Bt_H);
         bt = new Button(versus);
 
+        // setting = new JButton(new ImageIcon("TetrisCode/image/settings.png"));
         setting = new JButton(new ImageIcon("image\\settings.png"));
         setting.setBounds(250,180,Bt_W,Bt_H);
         bt = new Button(setting);
 
+        // tutorial = new JButton(new ImageIcon("TetrisCode/image/Tetris.png"));
         tutorial = new JButton(new ImageIcon("image\\Tetris.png"));
         tutorial.setBounds(30,100,150,98);
         bt = new Button(tutorial);
@@ -94,16 +112,23 @@ public class Select extends JFrame {
         setting.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                Setting st = null;
+                Setting st;
                 setVisible(false);
                 try {
                     st = new Setting();
                     st.setVisible(true);
-                } catch (IOException ex) {
-                    throw new RuntimeException(ex);
-                }
+                } catch (IOException e1) {
+                    // TODO Auto-generated catch block
+                    e1.printStackTrace();
+                } 
             }
         });
+    }
+    
+    class BackG extends JPanel { // Panel에 배경화면을 나타내기 위해서,
+        public void paint(Graphics g){
+            g.drawImage(img, 0,0,null);
+        }
     }
 
 }
