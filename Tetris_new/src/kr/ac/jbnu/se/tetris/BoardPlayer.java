1304e9318a02c40ee2097406e57dca36f2ac3d02
kkh0920 <rudgns1535@gmail.com>
2023-09-26 11:55:39 +0900

    플레이어용 보드 (Board 상속)

--- Tetris_new/src/kr/ac/jbnu/se/tetris/BoardPlayer.java
+++ Tetris_new/src/kr/ac/jbnu/se/tetris/BoardPlayer.java
@@ -9,7 +9,7 @@ public class BoardPlayer extends Board {
     
     public BoardPlayer(Tetris parent) {
         super(parent);
-        //TODO Auto-generated constructor stub
+        //TODO Auto-generated constructor stub 
         
         player = new Player();
         timer = new Timer(400, this);
