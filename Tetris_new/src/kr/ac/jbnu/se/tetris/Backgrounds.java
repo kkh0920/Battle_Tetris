b8a6723fa926825f343c0705f8969b0ebb8d68e8
kkh0920 <rudgns1535@gmail.com>
2023-09-27 15:43:14 +0900

    배경 화면

--- Tetris_new/src/kr/ac/jbnu/se/tetris/Backgrounds.java
+++ Tetris_new/src/kr/ac/jbnu/se/tetris/Backgrounds.java
@@ -8,29 +8,36 @@ import java.io.File;
 import java.io.IOException;
 
 public class Backgrounds extends JFrame {
-    int Frame_X = 430, Frame_Y = 336;
-    BufferedImage img = null;
-    BackG background;
-    JLayeredPane frame = new JLayeredPane();
+
+    private BufferedImage img = null;
+    
+    private BackG background;
+    
+    private JLayeredPane layeredPane = new JLayeredPane();
+    
     Backgrounds() throws IOException {
         DrawBackGround();
     }
 
-    public void DrawBackGround() throws IOException { // LayerdPane에 이미지를 덮어씌우는 메소드
-        frame.setSize(Frame_X,Frame_Y);
-        frame.setLayout(null);
+    public JLayeredPane getPane(){
+        return layeredPane;
+    }
+
+    private void DrawBackGround() throws IOException { // LayerdPane에 이미지를 덮어씌우는 메소드
+        layeredPane.setSize(Select.Frame_X, Select.Frame_Y);
+        layeredPane.setLayout(null);
 
         // img = ImageIO.read(new File("TetrisCode/image/backg.png"));
         img = ImageIO.read(new File("image\\backg.png"));
 
         background = new BackG();
-        background.setSize(Frame_X,Frame_Y);
-        frame.add(background);
+        background.setSize(Select.Frame_X, Select.Frame_Y);
+        layeredPane.add(background);
     }
 
     class BackG extends JPanel { // Panel에 이미지를 나타내기 위해서,
         public void paint(Graphics g){
-            g.drawImage(img, 0,0,null);
+            g.drawImage(img, 0, 0, null);
         }
     }
 }
