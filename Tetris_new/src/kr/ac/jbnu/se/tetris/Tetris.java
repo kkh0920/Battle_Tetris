ddab746a918ce3f2aee94d941dbc49de5287346e
kkh0920 <rudgns1535@gmail.com>
2023-09-28 01:46:29 +0900

    게임 한 개 담당

--- Tetris_new/src/kr/ac/jbnu/se/tetris/Tetris.java
+++ Tetris_new/src/kr/ac/jbnu/se/tetris/Tetris.java
@@ -8,21 +8,24 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class Tetris extends JPanel {
+    
+    private TetrisGameManager parent;
 
     private JLabel statusbar;
     
     private Board board;
-    
-    private boolean isComputer;
 
-    public int[][] nextPiece;
+    private JPanel panel;
 
-    public Tetris(boolean isComputer) throws CloneNotSupportedException {
-        setPreferredSize(new Dimension(200, 450));
+    public Tetris(TetrisGameManager parent, boolean isComputer) throws CloneNotSupportedException {
+        setPreferredSize(new Dimension(350, 450));
         setBackground(new Color(230, 230, 230));
-        
-        this.isComputer = isComputer;
 
+        this.parent = parent;
+
+        panel = new JPanel();
+        panel.setPreferredSize(new Dimension(75, 120));
+    
         statusbar = new JLabel("0");
 
         if(isComputer)
@@ -30,20 +33,20 @@ public class Tetris extends JPanel {
         else
             board = new BoardPlayer(this);
 
-        nextPiece = new int[4][2];
+        panel.add(board.getBlockPreview(), BorderLayout.NORTH);
+        panel.add(statusbar, BorderLayout.SOUTH);
 
-        add(board);
-        add(statusbar, BorderLayout.SOUTH);
+        add(board, BorderLayout.WEST);
+        add(panel, BorderLayout.EAST);
     }
-    
+    public TetrisGameManager gameManager(){
+        return parent;
+    }
     public JLabel getStatusBar() {
         return statusbar;
     }
     public Board getBoard() {
         return board;
-    }
-    public boolean isComputer(){
-        return isComputer;
     }
 }
 
