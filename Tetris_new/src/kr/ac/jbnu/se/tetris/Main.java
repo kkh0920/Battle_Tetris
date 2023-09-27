2b36f5e15a146dcab4bc3cda4b84d3eb459f0d0d
kkh0920 <rudgns1535@gmail.com>
2023-09-27 15:44:12 +0900

    실행부

--- Tetris_new/src/kr/ac/jbnu/se/tetris/Main.java
+++ Tetris_new/src/kr/ac/jbnu/se/tetris/Main.java
@@ -2,8 +2,12 @@ package kr.ac.jbnu.se.tetris;
 
 import java.io.IOException;
 
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+
 public class Main {
-    public static void main(String[] args) throws IOException {
-        new Select(); // 메인
+    public static void main(String[] args) throws IOException, UnsupportedAudioFileException, LineUnavailableException {
+        Select home = new Select(); // 메인
+        home.setVisible(true);
     }
 }
