f76d9675bd3915a9307fd86ee34d57b2bf607831
kkh0920 <rudgns1535@gmail.com>
2023-09-27 15:44:33 +0900

    환경 설정

--- Tetris_new/src/kr/ac/jbnu/se/tetris/Setting.java
+++ Tetris_new/src/kr/ac/jbnu/se/tetris/Setting.java
@@ -4,31 +4,34 @@ package kr.ac.jbnu.se.tetris;
 // import sun.audio.AudioPlayer;
 // import sun.audio.AudioStream;
 
-import javax.sound.sampled.*;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.*;
 
-
-
 public class Setting extends JFrame {
 
-    Backgrounds backgrounds;
-    JButton musicOn,musicOff,changekey,backselect;
+    private Select home;
+
+    private Backgrounds backgrounds;
+    private JButton musicButton, changekey, backselect;
+    private boolean isMusicStarted = true;
+
+    private KeyChange keyChange;
+
     Button bt;
 
-    Music music;
-
-    Setting() throws IOException, LineUnavailableException, UnsupportedAudioFileException {
+    Setting(Select home) throws IOException {
+        this.home = home;
+        keyChange = new KeyChange();
         setFrame();
     }
 
-    public void setFrame() throws IOException, LineUnavailableException, UnsupportedAudioFileException {
+    public void setFrame() throws IOException {
         backgrounds = new Backgrounds();
         setButton();
-        add(musicOn); add(musicOff); add(changekey); add(backselect); add(backgrounds.frame);
-        setSize(430,336);
+        add(musicButton); add(changekey); add(backselect); add(backgrounds.getPane());
+        setSize(Select.Frame_X, Select.Frame_Y);
         setLayout(null);
         setResizable(false);
         setLocationRelativeTo(null);
@@ -36,28 +39,21 @@ public class Setting extends JFrame {
     }
 
     public void addButton() {
+        // musicButton = new JButton(new ImageIcon("TetrisCode/image/musicOn.png"));
+        musicButton = new JButton(new ImageIcon("image\\musicOn.png"));
 
-        // musicOn = new JButton(new ImageIcon("TetrisCode/image/musicOn.png"));
-        musicOn = new JButton(new ImageIcon("image\\musicOn.png"));
-
-        // musicOff = new JButton(new ImageIcon("TetrisCode/image/musicOff.png"));
-        musicOff = new JButton(new ImageIcon("image\\musicOff.png"));
+        changekey = new JButton("키 변경");
 
         // backselect = new JButton(new ImageIcon("TetrisCode/image/back.png"));
         backselect = new JButton(new ImageIcon("image\\back.png"));
     }
 
-    public void setButton() throws UnsupportedAudioFileException, LineUnavailableException, IOException {
+    public void setButton() {
         addButton();
-        music = new Music();
+        
+        bt = new Button(musicButton);
+        musicButton.setBounds(50, 60, 100, 100);
 
-        bt = new Button(musicOn);
-        musicOn.setBounds(50, 60, 100, 100);
-
-        bt = new Button(musicOff);
-        musicOff.setBounds(50, 180, 100, 100);
-
-        changekey = new JButton("키 변경");
         changekey.setBounds(250, 80, 150, 150);
 
         bt = new Button(backselect);
@@ -65,36 +61,39 @@ public class Setting extends JFrame {
 
         buttonAction();
     }
-    public void buttonAction() {
 
+    private void pauseMusic(){
+        isMusicStarted = !isMusicStarted;
+        if(isMusicStarted){
+            // musicButton.setIcon(new ImageIcon("TetrisCode/image/musicOn.png"));
+            musicButton.setIcon(new ImageIcon("image\\musicOn.png"));
+            home.getMusic().startMusic();
+        }
+        else{
+            // musicButton.setIcon(new ImageIcon("TetrisCode/image/musicOff.png"));
+            musicButton.setIcon(new ImageIcon("image\\musicOff.png"));
+            home.getMusic().stopMusic();
+        }
+    }
+
+    public void buttonAction() {
+            musicButton.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    pauseMusic();
+                }
+            });
             changekey.addActionListener(new ActionListener() {
                 @Override
                 public void actionPerformed(ActionEvent e) {
-                    new KeyChange();
+                    keyChange.setVisible(true);
                 }
             });
-            musicOn.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    music.startMusic();
-                }
-            });
-            musicOff.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    music.stopMusic();
-                }
-            });
-
             backselect.addActionListener(new ActionListener() {
                 @Override
-                public void actionPerformed(ActionEvent e) {
-                    try {
-                        new Select();
-                        setVisible(false);
-                    } catch (IOException ex) {
-                        throw new RuntimeException(ex);
-                    }
+                public void actionPerformed(ActionEvent e) {           
+                    setVisible(false);
+                    home.setVisible(true);
                 }
             });
         }
