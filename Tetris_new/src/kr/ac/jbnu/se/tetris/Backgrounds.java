820cf861380355b499f1ea311fcf93f49a35b4e4
kkh0920 <rudgns1535@gmail.com>
2023-10-06 20:24:34 +0900

    배경화면 -> 매개변수로 경로 전달

--- Tetris_new/src/kr/ac/jbnu/se/tetris/Backgrounds.java
+++ Tetris_new/src/kr/ac/jbnu/se/tetris/Backgrounds.java
@@ -39,7 +39,7 @@ public class Backgrounds extends JFrame {
         layeredPane.add(background);
     }
 
-    class BackG extends JPanel { // Panel에 이미지를 나타내기 위해서,
+    class BackG extends JPanel { // Panel에 이미지를 나타내기 위해서
         public void paint(Graphics g){
             g.drawImage(img, 0, 0, null);
         }
