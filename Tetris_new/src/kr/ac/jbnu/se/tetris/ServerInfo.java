26f06dc84841498bc0e72b7edeebb7a8dc52e003
Jeongbright <wjswjdgus333@gmail.com>
2023-10-10 20:09:26 +0900

    .

--- Tetris_new/src/kr/ac/jbnu/se/tetris/ServerInfo.java
+++
@@ -1,8 +0,0 @@
-package kr.ac.jbnu.se.tetris;
-
-public interface ServerInfo {
-    public static final String SERVER_URL = "jdbc:mysql://tetris.cjrrqj3pg3pd.ap-northeast-2.rds.amazonaws.com:3306/USER_INFO";
-    public static final String MANAGEMENT_USER = "admin";
-    public static final String MANAGEMENT_PASSWORD = "xpxmfltm";
-
-}
