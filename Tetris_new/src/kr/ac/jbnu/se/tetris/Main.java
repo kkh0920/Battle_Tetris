6d2c9fd7ac279e82c22d963d4950455f3275f855
kkh0920 <rudgns1535@gmail.com>
2023-10-14 17:10:37 +0900

    실행부

--- Tetris_new/src/kr/ac/jbnu/se/tetris/Main.java
+++ Tetris_new/src/kr/ac/jbnu/se/tetris/Main.java
@@ -1,15 +1,9 @@
 package kr.ac.jbnu.se.tetris;
 
-import java.io.IOException;
-import java.sql.SQLException;
-
-import javax.sound.sampled.LineUnavailableException;
-import javax.sound.sampled.UnsupportedAudioFileException;
-
 public class Main { 
-    public static void main(String[] args) throws SQLException, UnsupportedAudioFileException, LineUnavailableException {
-/*        LoginPage login = new LoginPage();
-        login.setVisible(true);*/
+    public static void main(String[] args) {
+        // LoginPage login = new LoginPage();
+        // login.setVisible(true);
         Select select = new Select();
         select.setVisible(true);
     }
