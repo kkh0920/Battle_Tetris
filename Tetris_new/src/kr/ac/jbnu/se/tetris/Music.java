b4f181d77202fbd8d204246188198e77edace637
Jeongbright <wjswjdgus333@gmail.com>
2023-10-15 02:41:53 +0900

    volume control

--- Tetris_new/src/kr/ac/jbnu/se/tetris/Music.java
+++ Tetris_new/src/kr/ac/jbnu/se/tetris/Music.java
@@ -1,9 +1,6 @@
 package kr.ac.jbnu.se.tetris;
 
-import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.Clip;
-import javax.sound.sampled.LineUnavailableException;
-import javax.sound.sampled.UnsupportedAudioFileException;
+import javax.sound.sampled.*;
 import java.io.File;
 import java.io.IOException;
 
@@ -27,12 +24,23 @@ public class Music {
         }
     }
 
+    public void setVolume(float volume){
+        if(clip != null){
+            if (volume < 0f) volume = 0f;
+            else if (volume > 1f) volume = 1f;
+            FloatControl VolumeControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+            float dB = (float) (Math.log(volume) / Math.log(10.0) * 20.0);
+            VolumeControl.setValue(dB);
+        }
+    }
+
     public void stopMusic() {
         clip.stop();
     }
 
     public void startMusic() {
         clip.start();
+        setVolume(0.3f);
         clip.loop(10);
     }
 
