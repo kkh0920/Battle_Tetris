6fd03c309461ccb0588f7d0b285e3fa4ff0d63a5
kkh0920 <rudgns1535@gmail.com>
2023-10-24 10:09:56 +0900

    Test Branch Merge

--- src/kr/ac/jbnu/se/tetris/Main.java
+++ src/kr/ac/jbnu/se/tetris/Main.java
@@ -4,10 +4,10 @@ import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
-        LoginPage login = new LoginPage();
-        login.setVisible(true);
+        // LoginPage login = new LoginPage();
+        // login.setVisible(true);
 
-        // Select select = new Select();
-        // select.setVisible(true);
+        Select select = new Select();
+        select.setVisible(true);
     }
 }
