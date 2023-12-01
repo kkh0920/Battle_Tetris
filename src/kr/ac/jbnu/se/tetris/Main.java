153ae70941d0c365b2967ad02dbc8d098d2f4c0c
jeongbright <104780993+Jeong-Bright@users.noreply.github.com>
2023-12-01 22:16:34 +0900

    수정

--- src/kr/ac/jbnu/se/tetris/Main.java
+++ src/kr/ac/jbnu/se/tetris/Main.java
@@ -2,14 +2,15 @@ package kr.ac.jbnu.se.tetris;
 
 import java.sql.SQLException;
 
+import kr.ac.jbnu.se.tetris.server.LoginPage;
 import kr.ac.jbnu.se.tetris.ui.Select;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
-        // LoginPage login = new LoginPage();
-        // login.setVisible(true);
+        LoginPage login = new LoginPage();
+        login.setVisible(true);
 
-        Select select = new Select();
-        select.setVisible(true);
+/*        Select select = new Select();
+        select.setVisible(true);*/
     }
 }
