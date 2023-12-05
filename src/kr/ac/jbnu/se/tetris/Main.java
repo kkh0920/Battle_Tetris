c10191944d5103e85f6051691c6c169aae007c5c
jeongbright <104780993+Jeong-Bright@users.noreply.github.com>
2023-12-05 20:26:47 +0900

    main에서의 login page 제거 (과제 제출 끝)

--- src/kr/ac/jbnu/se/tetris/Main.java
+++ src/kr/ac/jbnu/se/tetris/Main.java
@@ -7,10 +7,11 @@ import kr.ac.jbnu.se.tetris.ui.Select;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
-        LoginPage login = new LoginPage();
-        login.setVisible(true);
+/*        LoginPage login = new LoginPage();
+        login.setVisible(true);*/
 
-/*        Select select = new Select();
-        select.setVisible(true);*/
+        // 보안상 오픈된 환경에서는 로그인 X
+        Select select = new Select();
+        select.setVisible(true);
     }
 }
